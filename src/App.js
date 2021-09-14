4d8864e76cedf044e174d09272bcc9623c140dd5
Md Polash Miya <polashmiya2015@gmail.com>
2021-09-14 21:29:49 +0600

    first comiit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import data from "./data";
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count < 0) {
+      amount = 1;
+    }
+    if (count > data.length) {
+      amount = data.length;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <h3>Generate Some Names</h3>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="amount"> Amount : </label>
+        <input
+          type="number"
+          name="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit">Generate</button>
+      </form>
+      {text.map((name, index) => {
+        return <p key={index}>{name}</p>;
+      })}
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,15 @@
+//here you can add more and more text,paragraph etc
+const text = [
+  "it is just a simple text for generator one",
+  "it is just a simple text for generator two",
+  "it is just a simple text for generator three",
+  "it is just a simple text for generator four",
+  "it is just a simple text for generator five",
+  "it is just a simple text for generator six",
+  "it is just a simple text for generator seven",
+  "it is just a simple text for generator eight",
+  "it is just a simple text for generator nine",
+  "it is just a simple text for generator ten",
+  "it is just a simple text for generator eleven",
+];
+export default text;
